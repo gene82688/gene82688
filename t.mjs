366c48809620b138e1bf077408341239d037cd90
gene82688 <168088876+gene82688@users.noreply.github.com>
2024-06-05 14:19:45 +0900

    Create t.mjs

---
+++ t.mjs
@@ -0,0 +1,533 @@
+const tasks = [
+  // 1_a_2
+{
+    name:  "BridgeImplementationevmChainId",
+    hashId: "QmefdCsA5eqd2PUbEKUThXKVcqbovKs5RPUoSvL71AgtzW",
+    fields: ["_clock", "_index", "evmchainid"],
+},
+// 2_a_2
+{
+    name:  "checkBalance1",
+    hashId: "QmfYEs7aUaw9BEBs41CotzXhkWiX9QC88GYkQBVKqoxpQD",
+    fields: ["_clock", "_index", "balanceof"],
+},
+// 3_a_2
+{
+    name:  "wwjajczruzjskuxytchku9salatest",
+    hashId: "QmZYsCSREtSfTEabR7VA7X2HqA2yiYriDgb5fjKUNVK5zw",
+    fields: ["_clock", "_index", "billingaccesscontroller"],
+},
+// 4_a_2
+{
+    name:  "MemecoinStakingsymbol",
+    hashId: "QmZT4Uh9Pfqrfszc6QDUub4ZcFeKYDiWg5CTZRcKttnydN",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 6_a_2
+{
+    name:  "nganlebalanceOf",
+    hashId: "QmZSigEay6x3pPNtaSd8SmeF7uKYTBYcpeaWRyxntDC7rc",
+    fields: ["_clock", "_index", "balanceof"],
+},
+// 7_a_2
+{
+    name:  "zyo1ewp",
+    hashId: "QmcacdoT4yZocS7CAMg1eLHjKNDxdMXahpdkbBs8VSxQsL",
+    fields: ["_clock", "_index", "typeandversion"],
+},
+// 8_a_2
+{
+    name:  "Orvalsymbol",
+    hashId: "QmavKbRdsXN8zQWZamX2WbsCyQpwSbLENK5CZDNpAQzEak",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 9_a_2
+{
+    name:  "xczxcadasdaasname",
+    hashId: "QmQZe7R1Ca84yzPLkJEB4xLQsAz3KdbyGXKDpNGaZXAH8s",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 11_a_2
+{
+    name:  "fdfsdfds",
+    hashId: "QmaDF5m7ZQaqyJrbv55yQ2Gna4kS2F7XeTjHBcvN1zVkL4",
+    fields: ["_clock", "_index", "decimals"],
+},
+// 12_a_2
+{
+    name:  "gfgdfsgfdsgfdgds",
+    hashId: "QmTfQKLfrvF3kLnKZCmS3Z44btomiR4YviavK3mcTWBEtP",
+    fields: ["_clock", "_index", "manager"],
+},
+// 13_a_2
+{
+    name:  "caascowner",
+    hashId: "QmdS3JFbUdRAnQcvv69XFtq1s8SAQxRiBiYw3edyMnRgCn",
+    fields: ["_clock", "_index", "owner"],
+},
+// 14_a_2
+{
+    name:  "eqgzp6oybcy",
+    hashId: "QmU2cosi7KKKxSsAGU15Pft2PDbJHrce28yd9YEaJNdhCS",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 15_a_2
+{
+    name:  "gre677PERMIT_TYPEHASH",
+    hashId: "QmTpntozx1n845ZREn3MSvXpPzzyD3LqK2zoMSSR4LE9RG",
+    fields: ["_clock", "_index", "permit_typehash"],
+},
+// 16_a_2
+{
+    name:  "NativeOptimismStackNATIVE_OPTI",
+    hashId: "QmeozFSHqidEr2RirX3e1JNGoff45fvTF9H6YvuJ9mDgYN",
+    fields: ["_clock", "_index", "native_optimism_erc20_external_bridge_function_selector"],
+},
+// 17_a_2
+{
+    name:  "DOMAIN_SEPARATOR1",
+    hashId: "Qmd3LEbeTAbGzG6LVBfSmWiDEBkJAEeTuPVkMvgVJe7vqs",
+    fields: ["_clock", "_index", "domain_separator"],
+},
+// 18_a_2
+{
+    name:  "ctcbalanceOf",
+    hashId: "QmekdViBWh6YG58MckC4PzJaZk8CivmvuD6EuKwpctz791",
+    fields: ["_clock", "_index", "balanceof"],
+},
+// 22_a_2
+{
+    name:  "mwrdnb5tytqdhvfg",
+    hashId: "QmWmJ6y12rtZZK1hLPoH4QcdEWr1uWPbAWxhpkgTc5F8qT",
+    fields: ["_clock", "_index", "billingaccesscontroller"],
+},
+// 23_a_2
+{
+    name:  "mwrdnb5tytqdh",
+    hashId: "Qmd2E1PSfvSAcdgNGq1MNxHLPRg3yXWANoZCwXujASV9vs",
+    fields: ["_clock", "_index", "latestanswer"],
+},
+// 24_a_2
+{
+    name:  "wrdnb5tytqdhvfgnilatest",
+    hashId: "QmRgXc2yLA51pf6rDtZoaGjRTzDgSJtJp79PB9nPVjPfnc",
+    fields: ["_clock", "_index", "latestround"],
+},
+// 26_a_2
+{
+    name:  "RswETHdecimals",
+    hashId: "Qmab2gFutTM1ZHESw6vK7MEfLGaXChbsrptP6ko6vPDCnE",
+    fields: ["_clock", "_index", "decimals"],
+},
+// 27_a_2
+{
+    name:  "erNitchalssy",
+    hashId: "QmeLnT3qZr2JPxhPFyK79w2rEmUdgRd5u7sugUUWraTd7H",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 28_a_2
+{
+    name:  "LINKsymbol",
+    hashId: "QmWRD5KjhihvHvU2mGHUGwkPTom2B16C7pzKjdJK2vs8MT",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 29_a_2
+{
+    name:  "x11balanceOf",
+    hashId: "QmVQGP3f92qQUJonqv8vGk92CGjhyPMzrFQurRC111Smrh",
+    fields: ["_clock", "_index", "balanceof"],
+},
+// 31_a_2
+{
+    name:  "hehe1symbol",
+    hashId: "QmcaFfpbt3gTy4HjRL5HtQCYG2Ng5xke5uMMRrXkCw8zB8",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 32_a_2
+{
+    name:  "maicobalanceOf",
+    hashId: "QmaAwCcFXogGmPk1hJGKrt5sgtu4A1G8rDfTQVrpJVvJCT",
+    fields: ["_clock", "_index", "balanceof"],
+},
+// 33_a_2
+{
+    name:  "b7d6nszp0nh",
+    hashId: "QmdizA1HzYb6CyUNa5iKvXTrtSm925rty7euUULKYJcbHV",
+    fields: ["_clock", "_index", "latestround"],
+},
+// 34_a_2
+{
+    name:  "genesisERC1155Addre",
+    hashId: "QmSDnLkTNoaiHPGtDv54MfjgaFUq3XWcEqMA1Df2AkRuXP",
+    fields: ["_clock", "_index", "genesiserc1155address"],
+},
+// 35_a_2
+{
+    name:  "isExcludedFromFees",
+    hashId: "QmZ5E6Mnxx4ui9qUdKehxvZXGNq8RdR8ZVarQP1RvqNtsj",
+    fields: ["_clock", "_index", "isexcludedfromfees"],
+},
+// 36_a_2
+{
+    name:  "Zcoimname",
+    hashId: "QmSJhB1CNvwmV3BKxwAxo7oiUAD3bvEPQmg4vhLuMN4rC7",
+    fields: ["_clock", "_index", "name"],
+},
+// 37_a_2
+{
+    name:  "d9B9D828a48",
+    hashId: "QmfT5yWUVfrJi1m4JHYdfRBBWuYv2TF12Vx62RmYgTQSpm",
+    fields: ["_clock", "_index", "permit_typehash"],
+},
+// 38_a_2
+{
+    name:  "umcnunbsaekwbiisM",
+    hashId: "QmbUj9163qQgbCfDGdMiRE48FyCjxN4rZ2mqmY25Ef5DVs",
+    fields: ["_clock", "_index", "isminter"],
+},
+// 42_a_2
+{
+    name:  "F6793De7Fa943a2",
+    hashId: "QmaX13YscHTAWEHNxrRWw3CzBu9ru6AshMvgKmC1neKw5H",
+    fields: ["_clock", "_index", "minimum_liquidity"],
+},
+// 43_a_2
+{
+    name:  "asdxzcmarketingWallet",
+    hashId: "QmdGPQV2kcJYMjLzvWeaeRX1wqtEARJH7k6nRof9b5ZQBd",
+    fields: ["_clock", "_index", "marketingwallet"],
+},
+// 44_a_2
+{
+    name:  "asdxzcowner",
+    hashId: "QmQr7SbviqEWEaBkS4fNXNTqXyzJnpC7ZqGuJEF5w6bgm7",
+    fields: ["_clock", "_index", "owner"],
+},
+// 46_a_2
+{
+    name:  "TTTowner",
+    hashId: "QmbtzoFJgdbn6WoyhaGDeykBhhpzp85Thk6MZ8m6FZGr97",
+    fields: ["_clock", "_index", "owner"],
+},
+// 47_a_2
+{
+    name:  "TTT_taxBuy",
+    hashId: "QmcXHaJnAfWBVdKDB3heaTDLjZhQ5hiCEdjawUa1CczJfm",
+    fields: ["_clock", "_index", "taxbuy"],
+},
+// 48_a_2
+{
+    name:  "hgrgergrepaused",
+    hashId: "QmSurEkHQKEg5Cr5tdqG8c3QY48spBKvMZCd12P622pCeM",
+    fields: ["_clock", "_index", "paused"],
+},
+// 49_a_2
+{
+    name:  "hgrgergretokenURI",
+    hashId: "QmYTYcaG9zofMQJHuswoJbkXsjM1bY9BKJaYygQJoxypoh",
+    fields: ["_clock", "_index", "tokenuri"],
+},
+// 51_a_2
+{
+    name:  "ZKSleeeee",
+    hashId: "QmQUrUQvFYaRNj3pXAjnowhTXhoifmWzs9nwxh9fhqoepi",
+    fields: ["_clock", "_index", "l"],
+},
+// 52_a_2
+{
+    name:  "ZKS_ownerwww",
+    hashId: "QmRXPJyFoeyztNm5XAkyingM6dHsocyQKRosyaR1YqsA4Z",
+    fields: ["_clock", "_index", "owner"],
+},
+// 53_a_2
+{
+    name:  "ZKSbffffff",
+    hashId: "QmNVoiG9LNzfdg6NDAiazpCeLbtXoXqNqJE9ZdedEhLbqn",
+    fields: ["_clock", "_index", "b"],
+},
+// 54_a_2
+{
+    name:  "petoken0www",
+    hashId: "QmV2sdEfe6nTRVCS3MU8Y4Z5awA8Vt1cPqVYWrXRHuVdCq",
+    fields: ["_clock", "_index", "token0"],
+},
+// 55_a_2
+{
+    name:  "pePERMIT_TYPEHASH",
+    hashId: "QmWC7eYCgATqwvTFPRS54Qarfy1Jn7TsZPMiXrAMzxg43x",
+    fields: ["_clock", "_index", "PERMIT_TYPEHASH"],
+},
+// 56_a_2
+{
+    name:  "pesymbol",
+    hashId: "Qmbrh6ymBtiCbjy7Apq5bAqniWGpqTYUE1GVnuEuU9tfRD",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 57_a_2
+{
+    name:  "peDOMAIN_SEPARATOR",
+    hashId: "QmQW7h2kmeSpz2kM7KTEore9KDsZCTt3TpcZxiCK7gb8cp",
+    fields: ["_clock", "_index", "DOMAIN_SEPARATOR"],
+},
+// 58_a_2
+{
+    name:  "peprice0CumulativeLast",
+    hashId: "QmRdt4MWZfxNctM3jfBZvozLJrSjo4AEXp1VhopZKoYY12",
+    fields: ["_clock", "_index", "price0CumulativeLast"],
+},
+// 59_a_2
+{
+    name:  "peprice1CumulativeLast",
+    hashId: "Qmf1Qee8bc533ia1hzYNHXeDzEzsZKyDT2HnjQtBbrzK9y",
+    fields: ["_clock", "_index", "price1CumulativeLast"],
+},
+// 60_a_2
+{
+    name:  "AXL_maxTaxSwap",
+    hashId: "QmTbYJwCa3yzmaANKwWumq6mHatMsM7gckoCsJ6bySZXFb",
+    fields: ["_clock", "_index", "maxTaxSwap"],
+},
+// 61_a_2
+{
+    name:  "AXL_maxTxAmount",
+    hashId: "QmcYnnqgZrf328Vm4nDgfu6kQSii2n7f9U5x7e64zv4Zqk",
+    fields: ["_clock", "_index", "maxTxAmount"],
+},
+// 62_a_2
+{
+    name:  "AXL_maxWalletSize",
+    hashId: "QmTWqzMWWUb1wtpanYmFXkXyNkkJvdKKs91rW7rJ8Ye4d3",
+    fields: ["_clock", "_index", "maxWalletSize"],
+},
+// 63_a_2
+{
+    name:  "trumfeeGrowthGlobal0X128",
+    hashId: "QmbGs3DofeXDiA4YT22f7GJxBF4afYSC1uoJg4u1nV4iha",
+    fields: ["_clock", "_index", "feeGrowthGlobal0X128"],
+},
+// 64_a_2
+{
+    name:  "trumfeeGrowthGlobal1X128",
+    hashId: "QmTnmW1dzxjPu7WPHuHZvcx7LxorSR2DDEPGZjW8xvVhcS",
+    fields: ["_clock", "_index", "feeGrowthGlobal1X128"],
+},
+// 65_a_2
+{
+    name:  "ACQdecimals",
+    hashId: "QmVbKjN6r5wfUNrKcwNt12M5AbHRSBf8QxQaeZBTJN2K7h",
+    fields: ["_clock", "_index", "decimals"],
+},
+// 66_a_2
+{
+    name:  "ACQbalanceOf",
+    hashId: "QmbvJSQG4esTGc2RrjdU5CTDiZu6ihZvkUSFN19rFARZAi",
+    fields: ["_clock", "_index", "balanceOf"],
+},
+// 67_a_2
+{
+    name:  "AVRaddressCommunity",
+    hashId: "QmTMh4HRVX7HF7BTAungeRnMjarL99WUrLRQifNkKFs5rk",
+    fields: ["_clock", "_index", "addressCommunity"],
+},
+// 68_a_2
+{
+    name:  "qffffeMAX_GAS_PAYING_PROVER",
+    hashId: "QmR71JZosesBWsEMRghWdyjitiQi1rZUgJvaewGE8BLu6G",
+    fields: ["_clock", "_index", "MAX_GAS_PAYING_PROVER"],
+},
+// 69_a_2
+{
+    name:  "AVRaddressesNoFee",
+    hashId: "QmdQBwbPCNMj79XGrc9ErodgeUnDziw6tBMWJYizjxNvy5",
+    fields: ["_clock", "_index", "addressesNoFee"],
+},
+// 70_a_2
+{
+    name:  "AVRMAX_SUPPLY",
+    hashId: "QmXdNf7skzVv6MSLoB4vdYTP149hdM9rqVN4ssiAR4bpA9",
+    fields: ["_clock", "_index", "MAX_SUPPLY"],
+},
+// 71_a_2
+{
+    name:  "Sukiyakiname",
+    hashId: "QmcdCNMFo3n7c5kvKE4kiSKFLZ4QKxgTtrQzTKVudNwyYs",
+    fields: ["_clock", "_index", "name"],
+},
+// 72_a_2
+{
+    name:  "baphicamountRaisedEth",
+    hashId: "QmZHePVJdyjiToRzkV47UUsZXzp8Jcz3BLuWfidKj87J9R",
+    fields: ["_clock", "_index", "amountRaisedEth"],
+},
+// 73_a_2
+{
+    name:  "baphicEthTax",
+    hashId: "QmS7H4SA2NqYzZVaGHGiKzmUTsvz8kxNFQMbFGcidM4KFy",
+    fields: ["_clock", "_index", "EthTax"],
+},
+// 74_a_2
+{
+    name:  "KTVtotalSupply",
+    hashId: "QmXosDTpHvJNqWbGuKymY9dhwUiNYQZWedU8L1xk59ySjH",
+    fields: ["_clock", "_index", "totalSupply"],
+},
+// 75_a_2
+{
+    name:  "KTVsymbol",
+    hashId: "QmZ2xDbVg9B1TdubNdBcWTZ7AyLLyD3Ga19RMGeZuU3ezE",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 76_a_2
+{
+    name:  "KTVdecimals",
+    hashId: "QmXS5B5QW9UWzA1ArwPdtrZZnULu3HEww6oXmVjtahQ3tn",
+    fields: ["_clock", "_index", "decimals"],
+},
+// 77_a_2
+{
+    name:  "KTVname",
+    hashId: "QmR6J3o9gM4sZLFUmTZoTYRWHLb5eTBtgAR91jRjZwfngA",
+    fields: ["_clock", "_index", "name"],
+},
+// 78_a_2
+{
+    name:  "SungZierkekLast",
+    hashId: "QmfAsXf1uiJDpEw3YNJYkK66gAcgcJvepgLXW4M3jRcBC9",
+    fields: ["_clock", "_index", "kLast"],
+},
+// 79_a_2
+{
+    name:  "SungZierketoken0",
+    hashId: "QmRfaUVawctKEuZYFFvjVD3N1WjdBHar92yZmNvyB6fjSk",
+    fields: ["_clock", "_index", "token0"],
+},
+// 80_a_2
+{
+    name:  "wangfjt35logic",
+    hashId: "QmYMCxxrzy9dMb1hGbXo2xMvPBGeGNFmcQ7AMM94bBHxG2",
+    fields: ["_clock", "_index", "logic"],
+},
+// 81_a_2
+{
+    name:  "SungZierketoken1",
+    hashId: "QmVNkBmohtKQctnEi3ZerJS9jqsq1g5gNK4QqmdjUiQBYL",
+    fields: ["_clock", "_index", "token1"],
+},
+// 82_a_2
+{
+    name:  "SungZierkeMINIMUM_LIQUIDITY",
+    hashId: "QmfCvv3b2V47UkdgMnnNDBpAia2L6AqaeJEC5bHTid7mCc",
+    fields: ["_clock", "_index", "MINIMUM_LIQUIDITY"],
+},
+// 83_a_2
+{
+    name:  "sunfanshunihaomeiowner",
+    hashId: "QmdyGrY7JFR1GWZGpmRVNm8gCwWifkp4hVEmANfMHuthM3",
+    fields: ["_clock", "_index", "owner"],
+},
+// 84_a_2
+{
+    name:  "sunfanshunihaomeisymbol",
+    hashId: "QmPEdRPPwhCEiZQthwaySSMUXUXsYYB3CbxSa4HdzZNz6n",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 85_a_2
+{
+    name:  "onghicngomprice0CumulativeLast",
+    hashId: "QmNrTQrYFLivGBdrpUNqkQEPsjks4dRLeKkEorVVMMXDZQ",
+    fields: ["_clock", "_index", "price0CumulativeLast"],
+},
+// 86_a_2
+{
+    name:  "dungconghicngomnonces",
+    hashId: "QmSiTKvFP6hYHUAGf835WoNCHu6JJznnc8QQLgbRrZL5qs",
+    fields: ["_clock", "_index", "nonces"],
+},
+// 87_a_2
+{
+    name:  "dungconghicngomname",
+    hashId: "QmPKMJiVw8XtabGXABUZc3V3tWZ4uVYrsWpjN5vA5pv4ss",
+    fields: ["_clock", "_index", "name"],
+},
+// 88_a_2
+{
+    name:  "dungconghicngomMINIMUM_LIQUIDI",
+    hashId: "QmWkggabKGFezVGBr3ZTaRPXcXVg31LahZKZ6GPZXBvnqE",
+    fields: ["_clock", "_index", "MINIMUM_LIQUIDITY"],
+},
+// 89_a_2
+{
+    name:  "dungconghicngomtoken0",
+    hashId: "QmTpvB1ddLkW2wQkdVEG6cwD1QM9b7FgU4Mk3n46Rn7Yqv",
+    fields: ["_clock", "_index", "token0"],
+},
+// 90_a_2
+{
+    name:  "SADNESStotalSupply",
+    hashId: "QmdUgSEyxNfCY5UbQA5rvjMthLb7Mnhup6K59xgFGRecuf",
+    fields: ["_clock", "_index", "totalSupply"],
+},
+// 91_a_2
+{
+    name:  "ASDASFcentiSellTax",
+    hashId: "QmS4FDES8FxJzcYnfihpc1vFqowEY6tp193wkEBHrrZThx",
+    fields: ["_clock", "_index", "centiSellTax"],
+},
+// 92_a_2
+{
+    name:  "ASDASFowner",
+    hashId: "QmcC9e1N2snoG1BeypFFn154PRmEreTC93wdHqWJpYRE27",
+    fields: ["_clock", "_index", "owner"],
+},
+// 93_a_2
+{
+    name:  "ASDASFOptimization",
+    hashId: "QmZHRyv431H7UtLuHTzuE1UT8Uhx5vUDxcKx3VPCZqywhg",
+    fields: ["_clock", "_index", "Optimization"],
+},
+// 94_a_2
+{
+    name:  "ASDASFmaxWalletAmount",
+    hashId: "QmbLWncZ8TJPGQHk5Ru4fKgcZbhckfaFK7ng6qDMbXRwMH",
+    fields: ["_clock", "_index", "maxWalletAmount"],
+},
+// 95_a_2
+{
+    name:  "ASDASFisExcludedFromFees",
+    hashId: "QmYYeeRFdpFvzY6YHNm2xpZGA4xBYqFf55cXxtR3bketLY",
+    fields: ["_clock", "_index", "isExcludedFromFees"],
+},
+// 96_a_2
+{
+    name:  "ASDASFcentiBuyTax",
+    hashId: "QmY5YtMUm7M7DirLi16hKKJY169oPeKCZmver2Z9aSNC4f",
+    fields: ["_clock", "_index", "centiBuyTax"],
+},
+// 97_a_2
+{
+    name:  "KUIsymbol",
+    hashId: "QmUaFyL8UUZ8QXviXtFPFg4r1U65V4u27bvfoKfiLyqbFB",
+    fields: ["_clock", "_index", "symbol"],
+},
+// 98_a_2
+{
+    name:  "KUIowner",
+    hashId: "QmfXUJqm8C1nGudUxTaSRWm7uUWrR1waix6SdrJ4HFniTa",
+    fields: ["_clock", "_index", "owner"],
+},
+// 99_a_2
+{
+    name:  "KUIdecimals",
+    hashId: "QmZ8Q564frBTR51zPVxn4qGq9c8EC7fqPLLmYcKSE6oMWb",
+    fields: ["_clock", "_index", "decimals"],
+},
+// 100_a_2
+{
+    name:  "shigerencainame",
+    hashId: "QmRMmHR2f96k3RyuUo66qVf8hJwz8oCjfb8tKB3w2TpjKc",
+    fields: ["_clock", "_index", "name"],
+},
+
+];
+
+export default tasks;
